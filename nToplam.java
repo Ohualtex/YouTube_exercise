a4f2a79483a05f81864bba9a7c6da1edd182d010
Miraç Duran <mduran2341@gmail.com>
2025-11-29 12:51:26 +0300

    Update nToplam.java

--- nToplam.java
+++ nToplam.java
@@ -1,3 +1,5 @@
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Döngü ile çözüm
 import java.util.Scanner;
 
 public class Toplam1denN {
@@ -11,6 +13,29 @@ public class Toplam1denN {
         for (int i = 1; i <= n; i++) {
             toplam += i;
         }
+
+        System.out.println("1'den " + n + "'e kadar olan sayilarin toplami: " + toplam);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Formül ile çözüm
+import java.util.Scanner;
+
+public class Toplam1denNFormul {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("n degerini girin: ");
+        int n = scanner.nextInt();
+
+        if (n < 1) {
+            System.out.println("n pozitif ve en az 1 olmali.");
+            return;
+        }
+
+        // long kullanmak daha guvenli, cunku n buyuk olursa carpim tasabilir
+        long toplam = (long) n * (n + 1) / 2;
 
         System.out.println("1'den " + n + "'e kadar olan sayilarin toplami: " + toplam);
     }
