2eeabce6786b31c2f5ad894d09195c5df554327d
Miraç Duran <mduran2341@gmail.com>
2025-11-29 12:48:04 +0300

    Update nToplam.java

--- nToplam.java
+++ nToplam.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class Toplam1denN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("n degerini girin: ");
+        int n = scanner.nextInt();
+
+        int toplam = 0;
+        for (int i = 1; i <= n; i++) {
+            toplam += i;
+        }
+
+        System.out.println("1'den " + n + "'e kadar olan sayilarin toplami: " + toplam);
+    }
+}
