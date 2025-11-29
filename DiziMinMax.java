341d386dd02c340477194ea8cdd5a9322a749446
Miraç Duran <mduran2341@gmail.com>
2025-11-29 12:52:15 +0300

    Update DiziMinMax.java

--- DiziMinMax.java
+++ DiziMinMax.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
+
+public class DiziMinMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kac adet sayi gireceksiniz? ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Eleman sayisi pozitif olmali.");
+            return;
+        }
+
+        System.out.print("1. sayiyi girin: ");
+        int sayi = scanner.nextInt();
+
+        int enBuyuk = sayi;
+        int enKucuk = sayi;
+
+        for (int i = 2; i <= n; i++) {
+            System.out.print(i + ". sayiyi girin: ");
+            sayi = scanner.nextInt();
+
+            if (sayi > enBuyuk) {
+                enBuyuk = sayi;
+            }
+            if (sayi < enKucuk) {
+                enKucuk = sayi;
+            }
+        }
+
+        System.out.println("En buyuk sayi: " + enBuyuk);
+        System.out.println("En kucuk sayi: " + enKucuk);
+    }
+}
 
